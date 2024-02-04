caea5e85221897489c608825a2d7ad58fba7451f
Sidor4ik <isidorenya.lenovo@gmail.com>
2024-02-04 18:14:08 +0100

    feat: implemented exit fuctionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import yourPath from './utils/path.js'
+import readline from 'readline';
 
-
-const startProject = () => {
-
+//start func
+const startProject = async  () => {
 const args = process.argv.slice(2);
 const usernameArgIndex = args.findIndex(arg => arg.startsWith('--username='));
 const username = usernameArgIndex !== -1 ? args[usernameArgIndex].split('=')[1] : 'Guest';
 
 console.log(`Welcome to the File Manager, ${username}!`);
 
+
+
+// exit func
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+ });
+ process.on('SIGINT', () => {
+	process.stdout.write('\nПриложение завершено с помощью Ctrl+C\n');
+	rl.close();
+	process.exit();
+ });
+// Обработка ввода из консоли
+rl.on('line', (input) => {
+	if (input === '.exit') {
+	  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+	  process.exit();
+	}
+ });
+
+ await  yourPath()
 }
 
 startProject()
-yourPath()
